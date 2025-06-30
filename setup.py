1bc41e9a0de14124a32210a46c84988d7b209a24
nishio sayaka <nishio@nishionoMacBook-Air.local>
2025-06-30 20:35:41 +0900

    setup change

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@ from setuptools import setup, find_packages
 setup(
     name="web_search",
     version="0.1.0",
-    description="Web search utility with DuckDuckGo and PDF/HTML content extraction",
-    author="Nishio",
-    url="https://github.com/west0714/web_search",
-    packages=find_packages(where="src"),
-    package_dir={"": "src"},   
+    packages=find_packages(),
     install_requires=[
         "requests",
         "beautifulsoup4",
-        "pymupdf"
+        "PyMuPDF"
     ],
-    python_requires=">=3.7",
+    author="Nishio",
+    #author_email="your.email@example.com",
+    description="A tool to perform DuckDuckGo search and extract contents.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/west0714/web_search",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    python_requires='>=3.7',
 )
